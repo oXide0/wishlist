52ccd4ce20f33b0e84a72266ed3c2ea1bc9a73c9
nazar <nazar10fantom@gmail.com>
2025-06-21 19:50:13 +0200

    update tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\User;
+
 it('returns a successful response', function () {
-    $response = $this->get('/');
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->get('/');
 
     $response->assertStatus(200);
 });
